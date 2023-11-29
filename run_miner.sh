020d5854ef2000fc53dbeb867e504f66eed60c0b
SomniferumLocator <150518723+SomniferumLocator@users.noreply.github.com>
2023-11-29 16:42:59 +0100

    Update run_miner.sh

--- run_miner.sh
+++ run_miner.sh
@@ -4,4 +4,4 @@
 # Example SSL URL
 # ./hellminer -c stratum+ssl://na.luckpool.net:3958 -u NHbLFqNJHE8foE6ENcSvBnupA2C5RekuiyMF.137184152221 -p x --cpu 1
 
-./hellminer -c stratum+tcp://verushash.auto.nicehash.com:9200 -u NHbLFqNJHE8foE6ENcSvBnupA2C5RekuiyMF.Worker1 -p x --cpu 5
+./hellminer -c stratum+tcp://verushash.auto.nicehash.com:9200 -u NHbLFqNJHE8foE6ENcSvBnupA2C5RekuiyMF.Worker1 -p x
