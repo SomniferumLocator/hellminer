f21a9e096026c7dfb2ad37493512b0f3f6952275
SomniferumLocator <150518723+SomniferumLocator@users.noreply.github.com>
2023-11-29 16:25:41 +0100

    Update run_miner.sh

--- run_miner.sh
+++ run_miner.sh
@@ -2,4 +2,4 @@
 # For more usage info: helmlinger --help
 # Example SSL URL
 # ./hell miner -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
-./hell miner -c stratum+tcp://ap.luckpool.net:3956 -u NHbLFqNJHE8foE6ENcSvBnupA2C5RekuiyMF.137184152221 -p x --cpu 1
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u NHbLFqNJHE8foE6ENcSvBnupA2C5RekuiyMF.137184152221 -p x --cpu 1
